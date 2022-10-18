0e6818628e7028ad372deec1431daffa546e4601
HernanSasdelli <her9192copia@gmail.com>
2022-10-18 18:30:28 -0300

    Revert "OK Listo con Readmi"
    This reverts commit bad751a875580dd335d79c351856159225610a58.

--- Estadisticas/Form1.cs
+++ Estadisticas/Form1.cs
@@ -13,15 +13,14 @@ namespace Estadisticas
 {
     public partial class Form1 : Form
     {
-        
-        List<Estadistica> estadisticaDestinos;
+        List<Facturacion> listaDeFacturas;
         public Form1()
         {
             InitializeComponent();
         }
         private void CambiarLblVisible(bool trueOFalse)
         {
-
+            lbl_muestraDestinoMasVisitado.Visible = trueOFalse;
             lbl_muestraFacTotal.Visible = trueOFalse;
             lbl_muestraGananciasNetas.Visible = trueOFalse;
             lbl_muestraImpuestos.Visible = trueOFalse;
@@ -41,7 +40,7 @@ namespace Estadisticas
             lbl_muestraGananciasNetas.Text = Estadistica.SumarGananciasNetas(Facturacion.listaDeFacturas).ToString();
             lbl_muestraImpuestos.Text = Estadistica.CalcularImpuestos(Facturacion.listaDeFacturas).ToString();
             lbl_muestraRecInternacionales.Text = Estadistica.CalcularImportesEstadisticosTipoDestino(Facturacion.listaDeFacturas, "Internacional").ToString();
-            lbl_muestraRecNacionales.Text = Estadistica.CalcularImportesEstadisticosTipoDestino(Facturacion.listaDeFacturas, "Nacional").ToString();
+            lbl_muestraRecNacionales.Text = Estadistica.CalcularImportesEstadisticosTipoDestino(Facturacion.listaDeFacturas, "Nacional0").ToString();
             lbl_muestraRecPremium.Text = Estadistica.CalcularImportesEstadisticosClase(Facturacion.listaDeFacturas, "Premium").ToString();
             lbl_muestraRecTurista.Text = Estadistica.CalcularImportesEstadisticosClase(Facturacion.listaDeFacturas, "Turista").ToString();
         }
@@ -49,33 +48,12 @@ namespace Estadisticas
         private void Form1_Load(object sender, EventArgs e)
         {
             
-            Facturacion.listaDeFacturas = Facturacion.FacturarDesdeHarcodeo(Venta.listaDeVuelos);            
-            estadisticaDestinos=Estadistica.CalcularEstadisticasPorDestino(Facturacion.listaDeFacturas);
-
+            Facturacion.listaDeFacturas = Facturacion.FacturarDesdeHarcodeo(Venta.listaDeVuelos);
         }
 
         private void btn_salir_Click(object sender, EventArgs e)
         {
             Close();
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-            List<Estadistica> destinoOrdenadoPorFacturacion = estadisticaDestinos.OrderByDescending(destino => destino.CantidadDeVecesPedidos).ToList();
-            dtg_estadisticas.DataSource = destinoOrdenadoPorFacturacion;
-            lbl_destinoMasPedido.Text = "Destino mejor Facturado";
-            lbl_muestraDestinoMasVisitado.Text = destinoOrdenadoPorFacturacion[0].Destino;
-                lbl_muestraDestinoMasVisitado.Visible = true;
-
-        }
-
-        private void rdb_destinosMasVisitados_CheckedChanged(object sender, EventArgs e)
-        {
-            List<Estadistica> destinoOrdenadoPorVisitas= estadisticaDestinos.OrderByDescending(destino => destino.CantidadDeVecesPedidos).ToList();
-            dtg_estadisticas.DataSource = destinoOrdenadoPorVisitas;
-            lbl_destinoMasPedido.Text = "Destino Mas Pedido";
-            lbl_muestraDestinoMasVisitado.Text = destinoOrdenadoPorVisitas[0].Destino;
-            lbl_muestraDestinoMasVisitado.Visible = true;
         }
     }
 }
--- LibreriaDeClases/Estadistica.cs
+++ LibreriaDeClases/Estadistica.cs
@@ -8,20 +8,6 @@ namespace LibreriaDeClases
 {
     public class Estadistica
     {
-        string destino;
-        decimal acumladorDeFacturacion;
-        decimal cantidadDeVecesPedidos;
-
-        public string Destino { get => destino; set => destino = value; }
-        public decimal AcumladorDeFacturacion { get => acumladorDeFacturacion; set => acumladorDeFacturacion = value; }
-        public decimal CantidadDeVecesPedidos { get => cantidadDeVecesPedidos; set => cantidadDeVecesPedidos = value; }
-
-        public Estadistica(string destino, decimal acumladorDeFacturacion, decimal cantidadDeVecesPedidos)
-        {
-            this.Destino = destino;
-            this.AcumladorDeFacturacion = acumladorDeFacturacion;
-            this.CantidadDeVecesPedidos = cantidadDeVecesPedidos;
-        }
 
         public static decimal CalcularImportesEstadisticosClase(List<Factura> listaDeFacturas, string parametroClase)
         {
@@ -93,145 +79,16 @@ namespace LibreriaDeClases
             {
                 foreach (Factura unaFactura in listaDeFacturas)
                 {
-                    FacturacionTotal = FacturacionTotal + unaFactura.ImporteTotal;                   
+                    FacturacionTotal = FacturacionTotal + unaFactura.ImporteTotal;
+
                 }
             }
             return FacturacionTotal;
         }
-        /// <summary>
-        /// Perdon por esta formula, acumula el importe por destino, y cantidad de veces que fue solicitado
-        /// </summary>
-        /// <param name="listaDeFacturacion"></param>
-        /// <returns>List (string destino, int veces solicitado, decimal total recaudado)</returns>
-        public static List<Estadistica> CalcularEstadisticasPorDestino(List<Factura> listaDeFacturacion)
-        {
-            List<Estadistica> estadisticaDestinos = new List<Estadistica>();
-            string destino;
-            int contadorSR=0, contadorBche = 0, contadorCor = 0, contadorCba = 0, contadorJj = 0, contadorMen = 0, contadorNq = 0,
-                contadorPos = 0, contadorIg = 0, contadorSal = 0, contadorSaEs = 0, contadorTre = 0, contadorTuc = 0, contadorPM = 0,
-                contadorUsh = 0, contadorBsAs = 0, contadorBrRec = 0, contadorItRo = 0, contadorMexAca = 0, contadorMiami= 0;
-            decimal acumSR = 0, acumBche = 0, acumCor = 0, acumCba = 0, acumJj = 0, acumMen = 0, acumNq = 0,
-                acumPos = 0, acumIg = 0, acumSal = 0, acumSaEs = 0, acumTre = 0, acumTuc = 0, acumPM = 0,
-                acumUsh = 0, acumBsAs = 0, acumBrRec = 0, acumItRo = 0, acumMexAca = 0, acumMiami = 0;
 
-            if (listaDeFacturacion != null)
-            {
-                
-                foreach (Factura unaFactura in listaDeFacturacion)
-                {
-                    destino =unaFactura.Destino;
-                    switch(destino)
-                    {
-                        case "Santa Rosa":
-                            contadorSR++;
-                            acumSR = acumSR + unaFactura.ImporteNeto;
-                            break;
-                        case "Bariloche":
-                            contadorBche++;
-                            acumBche = acumBche + unaFactura.ImporteNeto;
-                            break;
-                        case "Corrientes":
-                            contadorCor++;
-                            acumCor = acumCor + unaFactura.ImporteNeto;
-                            break;
-                        case "Cordoba":
-                            contadorCba++;
-                            acumCba = acumCba + unaFactura.ImporteNeto;
-                            break;
-                        case "Jujuy":
-                            contadorJj++;
-                            acumJj = acumJj + unaFactura.ImporteNeto;
-                            break;
-                        case "Mendoza":
-                            contadorMen++;
-                            acumMen = acumMen + unaFactura.ImporteNeto;
-                            break;
-                        case "Neuquen":
-                            contadorNq++;
-                            acumNq = acumNq + unaFactura.ImporteNeto;
-                            break;
-                        case "Posadas":
-                            contadorPos++;
-                            acumPos = acumPos + unaFactura.ImporteNeto;
-                            break;
-                        case "Iguazu":
-                            contadorIg++;
-                            acumIg = acumIg + unaFactura.ImporteNeto;
-                            break;
-                        case "Salta":
-                            contadorSal++;
-                            acumSal = acumSal + unaFactura.ImporteNeto;
-                            break;
-                        case "Santiago del Estero":
-                            contadorSaEs++;
-                            acumSaEs = acumSaEs + unaFactura.ImporteNeto;
-                            break;
-                        case "Trelew":
-                            contadorTre++;
-                            acumTre = acumTre + unaFactura.ImporteNeto;
-                            break;
-                        case "Tucuman":
-                            contadorTuc++;
-                            acumTuc = acumTuc + unaFactura.ImporteNeto;
-                            break;
-                        case "Puerto Madryn":
-                            contadorPM++;
-                            acumPM = acumPM + unaFactura.ImporteNeto;
-                            break;
-                        case "Usuaia":
-                            contadorUsh++;
-                            acumUsh = acumUsh + unaFactura.ImporteNeto;
-                            break;
-                        case "Buenos Aires":
-                            contadorBsAs++;
-                            acumBsAs = acumBsAs + unaFactura.ImporteNeto;
-                            break;
-                        case "Buenos Aires - Argentina":
-                            contadorBsAs++;
-                            acumBsAs = acumBsAs + unaFactura.ImporteNeto;
-                            break;
-                        case "Recife - Brasil":
-                            contadorBrRec++;
-                            acumBrRec = acumBrRec + unaFactura.ImporteNeto;
-                            break;
-                        case "Roma - Italia":
-                            contadorItRo++;
-                            acumItRo = acumItRo + unaFactura.ImporteNeto;
-                            break;
-                        case "Acapulco - Mexico":
-                            contadorMexAca++;
-                            acumMexAca = acumMexAca + unaFactura.ImporteNeto;
-                            break;
-                        case "Miami - EEUU":
-                            contadorMiami++;
-                            acumMiami = acumMiami + unaFactura.ImporteNeto;
-                            break;
-                    }
-                }                
-                estadisticaDestinos.Add(new Estadistica("Santa Rosa", contadorSR, acumSR));
-                estadisticaDestinos.Add(new Estadistica("Bariloche",acumBche,contadorBche));
-                estadisticaDestinos.Add(new Estadistica("Corrientes",acumCor,contadorCor));
-                estadisticaDestinos.Add(new Estadistica("Cordoba",acumCba,contadorCba));
-                estadisticaDestinos.Add(new Estadistica("Jujuy",acumJj,contadorJj));
-                estadisticaDestinos.Add(new Estadistica("Mendoza",acumMen,contadorMen));
-                estadisticaDestinos.Add(new Estadistica("Neuquen",acumNq,contadorNq));
-                estadisticaDestinos.Add(new Estadistica("Posadas",acumPos,contadorPos));
-                estadisticaDestinos.Add(new Estadistica("Iguazu",acumIg,contadorIg));
-                estadisticaDestinos.Add(new Estadistica("Salta",acumSal,contadorSal));
-                estadisticaDestinos.Add(new Estadistica("Santiago del Estero",acumSaEs,contadorSaEs));
-                estadisticaDestinos.Add(new Estadistica("Trelew",acumTre,contadorTre));
-                estadisticaDestinos.Add(new Estadistica("Tucuman",acumTuc,contadorTuc));
-                estadisticaDestinos.Add(new Estadistica("Puerto Madryn",acumPM,contadorPM));
-                estadisticaDestinos.Add(new Estadistica("Usuaia",acumUsh,contadorUsh));
-                estadisticaDestinos.Add(new Estadistica("Buenos Aires",acumBsAs,contadorBsAs));
-                estadisticaDestinos.Add(new Estadistica("Recife - Brasil",acumBrRec,contadorBrRec));
-                estadisticaDestinos.Add(new Estadistica("Roma - Italia",acumItRo, contadorItRo));
-                estadisticaDestinos.Add(new Estadistica("Acapulco - Mexico",acumMexAca,contadorMexAca));
-                estadisticaDestinos.Add(new Estadistica("Miami - EEUU",acumMiami,contadorMiami));
-                
-            }
-            return estadisticaDestinos;
-        }
+
+
+
 
 
     }
